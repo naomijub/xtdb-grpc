a31830241d57e104d43cb6d13de7f5be1ab70854
Julia Naomi <jnboeira@outlook.com>
2022-02-22 22:30:34 -0600

    compile protos

--- project.clj
+++ project.clj
@@ -1,11 +1,17 @@
+(def protobuf-version "3.19.1")
+
 (defproject xtdb-grpc "0.0.1-SNAPSHOT"
   :description "XTDB gRPC Server"
   :url "http://github.com/naomijub/xtdb-grpc"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
+  :plugins [[com.appsflyer/lein-protodeps "1.0.3"]
+            [lein-cljfmt "0.8.0"]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.pedestal/pedestal.service "0.5.10"]
                  [com.xtdb/xtdb-core "1.20.0"]
+                 [com.appsflyer/pronto "2.0.10"]
+                 [com.google.protobuf/protobuf-java ~protobuf-version]
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
@@ -23,5 +29,13 @@
   ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "xtdb-grpc.server/run-dev"]}
                    :dependencies [[io.pedestal/pedestal.service-tools "0.5.10"]]}
+             :test {:dependencies [[io.pedestal/pedestal.service-tools "0.5.10"]]}
              :uberjar {:aot [xtdb-grpc.server]}}
-  :main ^{:skip-aot true} xtdb-grpc.server)
+  :main ^{:skip-aot true} xtdb-grpc.server
+  
+  :lein-protodeps {:output-path "src/java"
+                   :proto-version ~protobuf-version
+                   :repos {:examples {:repo-type :filesystem
+                                      :config {:path "."}
+                                      :proto-paths ["resources"]
+                                      :dependencies [resources]}}})
